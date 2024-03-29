d8875f64afc77d9f3e0ddb5d58a66a2437b73247
Rakesh Trimbake <trimbake.rakesh1@gmail.com>
2024-03-29 13:40:42 +0530

    unwanted code removed

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,11 +1,10 @@
 import React from "react";
 import CategoryList from "./CategoryList";
-import styles from './Category.module.css';
+import styles from "./Category.module.css";
 
 const Category = () => {
   return (
     <div className={styles.category}>
-    
       <div className={styles.category_heading}>Category List Details</div>
       <CategoryList />
     </div>
--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,23 +1,7 @@
-import styles from './Expense.module.css';
+import styles from "./Expense.module.css";
 
 const Expense = () => {
-    return <div className={styles.expense}>At the start of the 1989–90 season, while playing for Rest of India, Tendulkar scored an unbeaten century in an Irani Trophy match against Delhi.[62]
-
-    In the final of 1995 Ranji Trophy, Tendulkar, captaining Bombay, scored 140 and 139 versus Punjab.[63]
-    
-    In the 1995–96 Irani Cup, he captained Mumbai against Rest of India.[63] His first double century (204*) was for Mumbai while playing against the visiting Australian team at the Brabourne Stadium in 1998.[30][64] He is the only player to score a century on debut in all three of his domestic first-class tournaments (the Ranji, Irani, and Duleep Trophies).[65] Another double century was an innings of 233* against Tamil Nadu in the semi-finals of the 2000 Ranji Trophy, which he regards as one of the best innings of his career.[66][67][68]
-    
-    In total, Tendulkar was part of five Ranji Trophy finals, in which Mumbai won 4.[63]
-    
-    County cricket
-    In 1992, at the age of 19, Tendulkar became the first overseas-born player to represent Yorkshire, which prior to Tendulkar joining the team, never selected players, even UK-based, from outside Yorkshire.[30][Note 1] Selected for Yorkshire as a replacement for the injured Australian fast bowler Craig McDermott, Tendulkar played 16 first-class matches for the team and scored 1,070 runs at an average of 46.52.[69]
-    
-    Career
-    Further information: List of international cricket centuries by Sachin Tendulkar
-    Early tours
-    Raj Singh Dungarpur is credited for the selection of Tendulkar for the Indian tour of Pakistan in late 1989,[70] after one first class season.[71] The Indian selection committee had shown interest in selecting Tendulkar for the tour of the West Indies held earlier that year, but eventually did not select him, as they did not want him to be exposed to the dominant fast bowlers of the West Indies so early in his career.[citation needed]
-    
-    Tendulkar was the youngest player to debut for India in Tests at the age of 16 years an</div>
-}
+  return <div className={styles.expense}>Expense Page</div>;
+};
 
 export default Expense;
